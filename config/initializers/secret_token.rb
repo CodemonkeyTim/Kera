fc28b933d13e1603b54fe3b48dfa05eadfc1b0d3
Teemu Vainio <vaintee1@lpt.fi>
2012-08-26 23:25:27 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kera::Application.config.secret_token = '3abf4ff11afe638bb57f6550691656d7c2608063b1ef045df1705ac1cdd9fdc18a3f1b70b3b3dcae401712c02d8c94bcbb864a5c9b1376ca0bd84cb68d741717'
